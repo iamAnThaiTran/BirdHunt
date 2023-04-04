a69121172f5ee86902e85757b80f7b87febfdc8d
AnThaiTran <114248510+chathangtheduocemlaai@users.noreply.github.com>
2023-04-05 02:21:27 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,7 +5,15 @@
 #include <string>
 #include<vector>
 #include<math.h>
+#include <stdlib.h>
+#include <time.h>
+
 using namespace std ;
+
+
+int random(int minN, int maxN){
+ return minN + rand() % (maxN + 1 - minN);
+}
 
 const double SCREEN_WIDTH = 1200;
 const double SCREEN_HEIGHT = 600;
@@ -30,6 +38,7 @@ void close()
 	IMG_Quit();
 	SDL_Quit();
 }
+/* class Base */
 class Base
 {
 public:
@@ -45,6 +54,10 @@ public:
     void setAlpha( Uint8 alpha );
     void setrect(double x, double y) ;
     void render(SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE ) ;
+    double get_pos_x(){return mPosx;}; double get_pos_y(){return mPosy;};double get_width(){return mWidth;}; double get_height(){return mHeight;};
+
+
+
 protected:
     SDL_Texture *mTexture ;
     double mWidth , mHeight, mPosx, mPosy ;
@@ -118,30 +131,36 @@ void Base::render( SDL_Rect* clip, double angle, SDL_Point* center, SDL_Renderer
 	SDL_RenderCopyEx( grenderer, mTexture, clip, &renderQuad, angle, center, flip );
 
 }
+/*CLASS BULLET */
 class Bullet:public Base
 {
 public:
     Bullet() ;
     ~Bullet() ;
-    void setmove(bool l) ;
+    bool get_move() ;
+    void set_up_or_down(bool l);
     void MouseAction() ;
     void BulletMove() ;
     void SetVel(double x , double y ) ;
 private:
-    bool ismove ;
+    bool ismove, up_or_down ;
     double vel_bullet_x ;
     double vel_bullet_y ;
 
 
 
 };
+void Bullet::set_up_or_down(bool l)
+{
+    up_or_down = l ;
+}
 Bullet::Bullet()
 {
     vel_bullet_x = 0 ;
     vel_bullet_y = 0 ;
-    mPosx = 0 ;
+    mPosx = 1200 ;
     mPosy = 0 ;
-    ismove = false ;
+    ismove = true  ;
 
 }
 Bullet::~Bullet()
@@ -153,9 +172,9 @@ void Bullet::SetVel(double x, double y)
     vel_bullet_x = x ;
     vel_bullet_y = y ;
 }
-void Bullet::setmove(bool l)
+bool Bullet::get_move()
 {
-    ismove = l ;
+    return ismove ;
 }
 void Bullet::MouseAction()
 {
@@ -163,9 +182,29 @@ void Bullet::MouseAction()
 }
 void Bullet::BulletMove()
 {
+    if(up_or_down)
+    {
         mPosx += vel_bullet_x ;
         mPosy += vel_bullet_y ;
+        if(mPosx > 1200 || mPosx <0 || mPosy > 500 || mPosy <0)
+        {
+            ismove = false ;
+
+        }
+    }
+    else
+    {
+        mPosy += 10 ;
+        if(mPosx > 1200 || mPosx <0  || mPosy <0 || mPosy > 600)
+        {
+            ismove = false ;
+
+        }
+    }
 }
+
+/*CLASS MAINOBJECT */
+
 class MainObject: public Base
 {
 public:
@@ -257,7 +296,7 @@ void MainObject::HandleAction( SDL_Event &e )
         Bullet new_bul ;
         new_bul.setrect(mPosx,mPosy) ;
         new_bul.SetVel((x-mPosx)/abs(x-mPosx)*10  , (y-mPosy)/abs(x-mPosx)*10) ;
-        cout << (x-mPosx)/abs(x-mPosx)*10  << " " <<  (y-mPosy)/abs(x-mPosx)*10 << endl ;
+        //cout << (x-mPosx)/abs(x-mPosx)*10  << " " <<  (y-mPosy)/abs(x-mPosx)*10 << endl ;
 
         Bul.push_back(new_bul) ;
 
@@ -279,13 +318,245 @@ void MainObject::HandleMove()
     //If the dot went too far up or down
 
 }
+ MainObject human ;
+/* ClASS Threast */
+class Threat : public Base
+{
+public:
+
+    Threat() ;
+    ~Threat() ;
+    void threat_move() ;
+    vector<Bullet> Bul ;
+    int get_pos_cum() {return pos_cum;}
+private:
+    double vel_threat_x ;
+    double vel_threat_y ;
+    int pos_cum ;
+
+};
+Threat::Threat()
+{
+    mPosx = 1200 ;
+    mPosy = 200 ;
+}
+Threat::~Threat()
+{
+
+}
+void Threat::threat_move()
+{
+
+    if(mPosx == 0)
+    {
+        pos_cum = random(20,1180)/10*10 ;
+
+    }
+    mPosx += -10 ;
+    mPosy = random(mPosy -3, mPosy +3) ;
+    if(mPosx < 0)
+    {
+        mPosx = 1200 ;
+        mPosy = random(20,200) ;
+        //ismove = false ;
+    }
+    if(mPosx == pos_cum)
+    {
+        Bullet new_bul ;
+        new_bul.setrect(mPosx, mPosy) ;
+        new_bul.set_up_or_down(false) ;
+        Bul.push_back(new_bul) ;
+    }
+
+}
+bool CheckCollision( Threat object1,  Bullet object2)
+{
+  double left_a = object1.get_pos_x();
+  double right_a = object1.get_pos_x() + object1.get_width();
+  double top_a = object1.get_pos_y();
+  double bottom_a = object1.get_pos_y() + object1.get_height();
+
+  int left_b = object2.get_pos_x();
+  int right_b = object2.get_pos_x() + object2.get_width();
+  int top_b = object2.get_pos_y();
+  int bottom_b = object2.get_pos_y() + object2.get_height();
+
+  // Case 1: size object 1 < size object 2
+  if (left_a > left_b && left_a < right_b)
+  {
+    if (top_a > top_b && top_a < bottom_b)
+    {
+      return true;
+    }
+  }
+
+  if (left_a > left_b && left_a < right_b)
+  {
+    if (bottom_a > top_b && bottom_a < bottom_b)
+    {
+      return true;
+    }
+  }
+
+  if (right_a > left_b && right_a < right_b)
+  {
+    if (top_a > top_b && top_a < bottom_b)
+    {
+      return true;
+    }
+  }
+
+  if (right_a > left_b && right_a < right_b)
+  {
+    if (bottom_a > top_b && bottom_a < bottom_b)
+    {
+      return true;
+    }
+  }
+
+  // Case 2: size object 1 < size object 2
+  if (left_b > left_a && left_b < right_a)
+  {
+    if (top_b > top_a && top_b < bottom_a)
+    {
+      return true;
+    }
+  }
+
+  if (left_b > left_a && left_b < right_a)
+  {
+    if (bottom_b > top_a && bottom_b < bottom_a)
+    {
+      return true;
+    }
+  }
+
+  if (right_b > left_a && right_b < right_a)
+  {
+    if (top_b > top_a && top_b < bottom_a)
+    {
+      return true;
+    }
+  }
+
+  if (right_b > left_a && right_b < right_a)
+  {
+    if (bottom_b > top_a && bottom_b < bottom_a)
+    {
+      return true;
+    }
+  }
+
+  // Case 3: size object 1 = size object 2
+  if (top_a == top_b && right_a == right_b && bottom_a == bottom_b)
+  {
+    return true;
+  }
+
+  return false;
+}
+bool CheckCollision1( MainObject object1,  Bullet object2)
+{
+  double left_a = object1.get_pos_x();
+  double right_a = object1.get_pos_x() + object1.get_width();
+  double top_a = object1.get_pos_y();
+  double bottom_a = object1.get_pos_y() + object1.get_height();
+
+  int left_b = object2.get_pos_x();
+  int right_b = object2.get_pos_x() + object2.get_width();
+  int top_b = object2.get_pos_y();
+  int bottom_b = object2.get_pos_y() + object2.get_height();
+
+  // Case 1: size object 1 < size object 2
+  if (left_a > left_b && left_a < right_b)
+  {
+    if (top_a > top_b && top_a < bottom_b)
+    {
+      return true;
+    }
+  }
+
+  if (left_a > left_b && left_a < right_b)
+  {
+    if (bottom_a > top_b && bottom_a < bottom_b)
+    {
+      return true;
+    }
+  }
+
+  if (right_a > left_b && right_a < right_b)
+  {
+    if (top_a > top_b && top_a < bottom_b)
+    {
+      return true;
+    }
+  }
+
+  if (right_a > left_b && right_a < right_b)
+  {
+    if (bottom_a > top_b && bottom_a < bottom_b)
+    {
+      return true;
+    }
+  }
+
+  // Case 2: size object 1 < size object 2
+  if (left_b > left_a && left_b < right_a)
+  {
+    if (top_b > top_a && top_b < bottom_a)
+    {
+      return true;
+    }
+  }
+
+  if (left_b > left_a && left_b < right_a)
+  {
+    if (bottom_b > top_a && bottom_b < bottom_a)
+    {
+      return true;
+    }
+  }
+
+  if (right_b > left_a && right_b < right_a)
+  {
+    if (top_b > top_a && top_b < bottom_a)
+    {
+      return true;
+    }
+  }
+
+  if (right_b > left_a && right_b < right_a)
+  {
+    if (bottom_b > top_a && bottom_b < bottom_a)
+    {
+      return true;
+    }
+  }
+
+  // Case 3: size object 1 = size object 2
+  if (top_a == top_b && right_a == right_b && bottom_a == bottom_b)
+  {
+    return true;
+  }
+
+  return false;
+}
 
 
-
+/* Main */
 int main(int argc, char* argv[])
 {
     Base background ;
-    MainObject human ;
+
+    Threat threat[3] ;
+    threat[0] = *new Threat ;threat[1] = *new Threat ;threat[2] = *new Threat ;
+    for(int i = 0 ; i < 3 ; i ++)
+        {
+            threat[i].setrect(1200 + i *random(100,400),random(20,100)) ;
+
+        }
+    srand(time(NULL));
+
     init() ;
     background.setrect(0,0) ;
     human.setrect(600,423) ;
@@ -315,21 +586,57 @@ int main(int argc, char* argv[])
         //loadnhanvat
         if(LorR)
         {
-            human.loadfromfile("human64x91.png");
+            human.loadfromfile("mainnu.png");
 
         }
         else
         {
-            human.loadfromfile("human64x91flip.png");
+            human.loadfromfile("mainnu.png");
 
         }
         human.render() ;
+
+        for(int i = 0 ; i < 3 ; i ++)
+        {
+            threat[i].loadfromfile("rsz_conchim.png") ;
+            threat[i].threat_move() ;
+            threat[i].render() ;
+
+
+        for(int j = 0 ; j < threat[i].Bul.size() ; j ++ )
+        {
+            threat[i].Bul[j].loadfromfile("sphere.png") ;
+            threat[i].Bul[j].BulletMove() ;
+            threat[i].Bul[j].render() ;
+            if(CheckCollision1(human,threat[i].Bul[j]))
+            {
+                SDL_ShowSimpleMessageBox(0,"gameover","gameover",gwindow) ;
+            }
+        }
+        }
+
+
         for(double i = 0 ; i < human.Bul.size() ; i++)
         {
+
             human.Bul[i].loadfromfile("sphere.png") ;
+            human.Bul[i].set_up_or_down(true);
             human.Bul[i].BulletMove();
+            if(human.Bul[i].get_move())
             human.Bul[i].render() ;
-           // human.Bul.erase(human.Bul.begin() + i ) ;
+            if (CheckCollision(threat[2],human.Bul[i]))
+            {
+                //cout << "da va cham" << endl ;
+                human.Bul.erase(human.Bul.begin() + i ) ;
+            }
+
+            if(!human.Bul[i].get_move())
+            {
+                 human.Bul.erase(human.Bul.begin() + i ) ;
+                 //cout << "daxoa va conlai "<< human.Bul.size() << endl ;
+            }
+
+
         }
        //render
         SDL_RenderPresent( grenderer );
